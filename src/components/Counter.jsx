e5268634028d434d1d3782cdbb8aa9226caf79ee
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-05-25 11:36:27 +0200

    added reset and setError

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react';
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [error, setError] = useState('');
 
   function Increment() {
+    setError('');
     setCount(count + 1);
   }
 
   function Decrement() {
-    setCount(count - 1);
+    if (count === 0) {
+      setError('Non posso andare sotto lo 0');
+      return;
+    } else {
+      setCount(count - 1);
+    }
+  }
+
+  function reset() {
+    setError('');
+    setCount(0);
   }
 
   return (
@@ -19,9 +31,18 @@ function Counter() {
           +
         </button>
 
-        <button className="btn btn-secondary" onClick={Decrement}>
+        <button className="btn  btn-primary" onClick={reset}>
+          Reset
+        </button>
+
+        <button className="btn mx-3 btn-secondary" onClick={Decrement}>
           -
         </button>
+        {error && (
+          <div className=" mt-3 rounded bg-danger text-white">
+            <p className="p-3">{error}</p>
+          </div>
+        )}
       </div>
     </div>
   );
